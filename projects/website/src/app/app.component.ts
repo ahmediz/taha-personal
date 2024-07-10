2c7bcf7bdd3def60b8c21c6a82c861da3e205a34
Ahmed Shaarawy <ahmed.shaarawy@nagwa.com>
2024-07-10 11:57:52 +0300

    adding initial code in website

--- projects/website/src/app/app.component.ts
+++ projects/website/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
-
+import { map, Observable } from 'rxjs';
+type Page = {
+  id: string;
+  title: string;
+  description: string;
+  color: string;
+};
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'website';
+  pagesList$: Observable<Page[]>;
+  firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'pages');
+    this.pagesList$ = collectionData(itemCollection, {
+      idField: 'id',
+    }).pipe(map((res) => res as Page[]));
+  }
 }

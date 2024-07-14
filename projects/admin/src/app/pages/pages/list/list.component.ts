8ef393326e5c9f3d945be2a1db01dff3e0cd6de9
Ahmed Shaarawy <ahmed.shaarawy@nagwa.com>
2024-07-14 16:29:00 +0300

    adding reordering items

--- projects/admin/src/app/pages/pages/list/list.component.ts
+++ projects/admin/src/app/pages/pages/list/list.component.ts
@@ -1,14 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import {
+  collection,
+  collectionData,
+  doc,
+  Firestore,
+  orderBy,
+  query,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { DndDropEvent } from 'ngx-drag-drop';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 type Page = {
   id: string;
   title: string;
   description: string;
   color: string;
+  order: number;
 };
 
 @Component({
@@ -17,13 +27,47 @@ type Page = {
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  pagesList$: Observable<Page[]>;
+  pagesList = signal<Page[]>([]);
   firestore = inject(Firestore);
+
+  onDragEnd(event: DragEvent) {
+    console.log('drag ended', JSON.stringify(event, null, 2));
+  }
+
+  async onDrop(event: DndDropEvent) {
+    const dropped = event.data;
+    let moved = this.pagesList()[event.index!];
+
+    if(!moved) {
+      moved = this.pagesList()[this.pagesList().length - 1]
+    }
+
+    moved.order = dropped.order;
+    dropped.order = event.index! + 1;
+
+    // Updating dropped
+    const docRef1 = doc(this.firestore, `pages/${dropped.id}`);
+    await updateDoc(docRef1, {
+      order: dropped.order,
+    });
+
+    // Updating Moved
+    const docRef2 = doc(this.firestore, `pages/${moved.id}`);
+    await updateDoc(docRef2, {
+      order: moved.order,
+    });
+  }
 
   constructor() {
     const itemCollection = collection(this.firestore, 'pages');
-    this.pagesList$ = collectionData(itemCollection, {
+    const q = query(itemCollection, orderBy('order'));
+    collectionData(q, {
       idField: 'id',
-    }).pipe(map((res) => res as Page[]));
+    })
+      .pipe(
+        map((res) => res as Page[]),
+        tap((res) => this.pagesList.set(res))
+      )
+      .subscribe();
   }
 }
--- projects/admin/src/app/pages/pages/pages.module.ts
+++ projects/admin/src/app/pages/pages/pages.module.ts
@@ -6,11 +6,20 @@ import { CoreModule } from '../../shared/modules/core.module';
 import { RouterLink } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { PageFormComponent } from './page-form/page-form.component';
-import { ButtonComponent } from "../../shared/components/button/button.component";
-import { InputFieldComponent } from "../../shared/components/input-field/input-field.component";
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { InputFieldComponent } from '../../shared/components/input-field/input-field.component';
+import { DndModule } from 'ngx-drag-drop';
 
 @NgModule({
   declarations: [ListComponent, PageFormComponent],
-  imports: [CommonModule, PagesRoutingModule, CoreModule, RouterLink, ButtonComponent, InputFieldComponent],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    CoreModule,
+    RouterLink,
+    ButtonComponent,
+    InputFieldComponent,
+    DndModule,
+  ],
 })
 export class PagesModule {}
